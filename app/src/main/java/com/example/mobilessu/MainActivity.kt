a10b1b862d6249f957d3a36e940a154530b77661
DeveloperAlex <stupak.99@inbox.ru>
2020-08-09 17:06:51 +0400

    add activity menu

--- app/src/main/java/com/example/mobilessu/MainActivity.kt
+++ app/src/main/java/com/example/mobilessu/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.mobilessu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main_student_faculty.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    //fun clickmenu(view: View){
+   //    val randomIntent = Intent(this, MainActivity_student_faculty::class.java)
+   //     startActivity(randomIntent)
+   // }
 }
