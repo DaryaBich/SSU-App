476961b84756acb5d9d17d93835f790fdedddde5
DeveloperAlex <stupak.99@inbox.ru>
2020-08-14 15:43:25 +0400

    add faculties in activity_student_faculty

--- app/src/main/java/com/example/mobilessu/student_faculty_page/Activity_student_faculty.kt
+++ app/src/main/java/com/example/mobilessu/student_faculty_page/Activity_student_faculty.kt
@@ -1,12 +1,16 @@
 package com.example.mobilessu.student_faculty_page
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.mobilessu.R
+import com.example.mobilessu.choose_course.Activity_course
+import com.example.mobilessu.choose_day_evening.Activity_day_evening
+import com.example.mobilessu.teacher_or_student_page.Activity_teacher_or_student
 import kotlinx.android.synthetic.main.activity_student_faculty.*
 
 class Activity_student_faculty : AppCompatActivity() {
@@ -26,14 +30,62 @@ class Activity_student_faculty : AppCompatActivity() {
 //=======
 
         setContentView(R.layout.activity_student_faculty)
-        val demodata = MutableList(5, {x -> "Item$x"})
-        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, demodata)
+            // val demodata = MutableList(2, {"Биологический факультет"; "Географический факультет"})
+        val demodata = mutableListOf<String>()
+        val demodata1 = mutableListOf<String>()
+
+        demodata.add("Биологический факультет")
+        demodata.add("Географический факультет")
+        demodata.add("Геологический факультет")
+        demodata.add("Институт дополнительного профессионального образования")
+        demodata.add("Институт искусств")
+        demodata.add("Институт истории и международных отношений")
+        demodata.add("Институт физической культуры и спорта")
+        demodata.add("Институт филологии и журналистики")
+        demodata.add("Институт химии")
+        demodata.add("Механико-математический факультет")
+        demodata.add("Социологический факультет")
+        demodata.add("Факультет иностранных языков и лингводидактики")
+        demodata.add("Факультет компьютерных наук и информационных технологий")
+        demodata.add("Факультет нано- и биомедицинских технологий")
+        demodata.add("Факультет нелинейных процессов")
+        demodata.add("Факультет психологии")
+        demodata.add("Факультет психолого-педагогического и специального образования")
+        demodata.add("Физический факультет")
+        demodata.add("Философский факультет")
+        demodata.add("Экономический факультет")
+        demodata.add("Юридический факультет")
+
+        demodata1.add("Геологический колледж")
+        demodata1.add("Колледж радиоэлектроники им. П.Н. Яблочкова")
+
+
+
+        val adapter = ArrayAdapter<String>(this, R.layout.list_item, demodata)
+        val adapter1 = ArrayAdapter<String>(this, R.layout.list_item, demodata1)
+
         listfaculty.adapter = adapter
+        listcolledge.adapter = adapter1
         listfaculty.setOnItemClickListener { adapterView, view, i, l ->
-            Toast.makeText(this, (view as TextView).text, Toast.LENGTH_LONG).show()
+            //Toast.makeText(this, (view as TextView).text, Toast.LENGTH_LONG).show()
+
+            click_faculty(view)
+        }
+        listcolledge.setOnItemClickListener { adapterView, view, i, l ->
+
+            click_colledge(view)
         }
 //>>>>>>> 92e20a2b554b26c93864bc01ba7368930bb4aa4c:app/src/main/java/com/example/mobilessu/student_faculty_page/Activity_student_faculty.kt
+    }
 
+    fun click_faculty(view: View){
+        val randomIntent = Intent(this, Activity_day_evening::class.java)
+        startActivity(randomIntent)
+    }
+
+    fun click_colledge(view: View){
+        val randomIntent = Intent(this, Activity_course::class.java)
+        startActivity(randomIntent)
     }
 
     fun click_back(view: View){
