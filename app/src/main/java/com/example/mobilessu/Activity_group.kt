0e9ef4330d56bb40f8545e71267db46e201a7f43
DeveloperAlex <stupak.99@inbox.ru>
2020-08-14 16:21:19 +0400

    add activity for groups

---
+++ app/src/main/java/com/example/mobilessu/Activity_group.kt
@@ -0,0 +1,16 @@
+package com.example.mobilessu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class Activity_group : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_group)
+    }
+
+    fun click_back(view: View){
+        finish()
+    }
+}
