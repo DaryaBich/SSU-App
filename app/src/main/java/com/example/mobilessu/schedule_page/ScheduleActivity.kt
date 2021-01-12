0c6b85195e391a24242853c1e3a403ef7d006040
devAlex <stupak.99@inbox.ru>
2021-01-12 21:01:56 +0300

    add functionality for buttons for replacing day of week

--- app/src/main/java/com/example/mobilessu/schedule_page/ScheduleActivity.kt
+++ app/src/main/java/com/example/mobilessu/schedule_page/ScheduleActivity.kt
@@ -3,7 +3,9 @@ package com.example.mobilessu.schedule_page
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.example.mobilessu.R
 import kotlinx.android.synthetic.main.activity_schedule.*
 import org.jsoup.Jsoup
@@ -56,7 +58,7 @@ class ScheduleActivity : AppCompatActivity() {
             val c = Calendar.getInstance()
             c.time = Date()
             val dayOfWeek = c[Calendar.DAY_OF_WEEK]
-            when (dayOfWeek - 2) {
+            when (dayOfWeek - 1) {
                 0 -> dayofweek.text = "Понедельник"
                 1 -> dayofweek.text = "Понедельник"
                 2 -> dayofweek.text = "Вторник"
@@ -68,9 +70,75 @@ class ScheduleActivity : AppCompatActivity() {
         }
     }
 
-
     fun click_back(view: View){
         finish()
         overridePendingTransition(R.anim.fide_in, R.anim.fide_out);
+    }
+
+    private fun returner(view: View): Int {
+        when (dayofweek.text) {
+            "Понедельник" -> return 1
+            "Вторник" -> return 2
+            "Среда" -> return 3
+            "Четверг" -> return 4
+            "Пятница" -> return 5
+            "Суббота" -> return 6
+            else -> return 1
+        }
+    }
+
+    fun openallbuttons(view: View) {
+        button_day1.isActivated = true
+        button_day2.isActivated = true
+        button_day3.isActivated = true
+        button_day4.isActivated = true
+        button_day5.isActivated = true
+        button_day6.isActivated = true
+    }
+
+//    fun updatelocationbuttons(view: View) {
+//        when(returner(view)) {
+//            1 -> {
+//                val params: RelativeLayout.LayoutParams = new RelativeLayout.LayoutParams
+//                params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT)
+//                button_day1.layoutParams = params
+//            }
+//        }
+//    }
+
+    fun click1(view: View){
+        dayofweek.text = "Понедельник"
+        openallbuttons(view)
+        button_day1.isActivated = false
+    }
+
+    fun click2(view: View){
+        dayofweek.text = "Вторник"
+        openallbuttons(view)
+        button_day2.isActivated = false
+    }
+
+    fun click3(view: View){
+        dayofweek.text = "Среда"
+        openallbuttons(view)
+        button_day3.isActivated = false
+    }
+
+    fun click4(view: View){
+        dayofweek.text = "Четверг"
+        openallbuttons(view)
+        button_day4.isActivated = false
+    }
+
+    fun click5(view: View){
+        dayofweek.text = "Пятница"
+        openallbuttons(view)
+        button_day5.isActivated = false
+    }
+
+    fun click6(view: View){
+        dayofweek.text = "Суббота"
+        openallbuttons(view)
+        button_day6.isActivated = false
     }
 }
